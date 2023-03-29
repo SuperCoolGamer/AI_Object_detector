34983a6774617031ab8302248f8da6561c9401b0
SuperCoolGamer <103328500+SuperCoolGamer@users.noreply.github.com>
2023-03-28 17:43:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+video=createVideo("video.mp4");
+
+}
+
+function setup(){
+
+    canvas = createCanvas(400,300);
+    canvas.center();
+    video.hide();
+    
+   video.size(400,300);
+}
+
+function draw(){
+
+}
